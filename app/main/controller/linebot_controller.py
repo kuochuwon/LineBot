97e5acb4a925f4dc801db1aef7e348da0917481d
Roykuo <kuochuwon@gmail.com>
2021-03-24 17:03:52 +0800

    linebot local ok

--- app/main/controller/linebot_controller.py
+++ app/main/controller/linebot_controller.py
@@ -1,4 +1,4 @@
-from app.main.dto.linebot import LineBotDto
+from app.main.dto.thelinebot import LineBotDto
 from app.main.service import ret
 from app.main.util.common import (aaa_verify, api_exception_handler,
                                   check_access_authority)
@@ -6,6 +6,7 @@ from flask import request
 from flask_api import status
 from flask_restplus import Resource
 from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from werkzeug.exceptions import NotFound
 
 api = LineBotDto.api
@@ -13,9 +14,29 @@ response_status = {status.HTTP_200_OK: ret.get_code_full(ret.RET_OK),
                    status.HTTP_401_UNAUTHORIZED: ret.get_code_full(ret.RET_NO_CUST_ID),
                    status.HTTP_404_NOT_FOUND: ret.get_code_full(ret.RET_NOT_FOUND)}
 
+CHANNEL_ACCESS_TOKEN = """
+qipp53w9dsKIjaDG3D5eYswChigJUmYdgD6ilha3BCHjF4rJmG8dVjj3kMqpBy4TvTnYODobZelFc5bsSz9ycEx09y
+/XU3aZO42Bp2o0+9f9TRJBFMeUih6Oi2YB77ET4+u5z/miOF5FRihh5ubRTgdB04t89/1O/w1cDnyilFU=
+"""
+
+CHANNEL_SECRET_TOKEN = """
+6fd6a21c86d311aaf115d9588cc5fc46
+"""
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET_TOKEN)
+
 
 @api.route("/webhook")
 class Webhook(Resource):
+    # 學你說話
+    @handler.add(MessageEvent, message=TextMessage)
+    def echo(event):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
     # @api.expect(_header, _get_all_device,
     #             validate=True)
     # @api.doc(responses=response_status)
@@ -24,10 +45,7 @@ class Webhook(Resource):
     @api_exception_handler
     def post(self):
         """ line bot response """
-        CHANNEL_ACCESS_TOKEN = """
-        qipp53w9dsKIjaDG3D5eYswChigJUmYdgD6ilha3BCHjF4rJmG8dVjj3kMqpBy4TvTnYODobZelFc5bsSz9ycEx09y
-        /XU3aZO42Bp2o0+9f9TRJBFMeUih6Oi2YB77ET4+u5z/miOF5FRihh5ubRTgdB04t89/1O/w1cDnyilFU=
-        """
+
         # to = "YOUR USER ID"
 
         line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
