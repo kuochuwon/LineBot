e6942e5cf0bf9185ba67e08be9be34f197732063
Roykuo <kuochuwon@gmail.com>
2021-04-12 21:39:42 +0800

    prototype of push and notify

--- app/main/constant.py
+++ app/main/constant.py
@@ -1,4 +1,28 @@
 
+class LineConstant:
+    CHANNEL_ACCESS_TOKEN = "qipp53w9dsKIjaDG3D5eYswChigJUmYdgD6ilha3BCHjF4rJmG8dVjj3kMqpBy4TvTnYODobZelFc5bsSz9ycEx09y/XU3aZO42Bp2o0+9f9TRJBFMeUih6Oi2YB77ET4+u5z/miOF5FRihh5ubRTgdB04t89/1O/w1cDnyilFU="
+    CHANNEL_SECRET_TOKEN = """
+    6fd6a21c86d311aaf115d9588cc5fc46
+    """
+    # CHANNEL_NOTIFY_TOKEN = "r2LutziDSljYNL2O0rq3kiBNn90lQCiQC7CATPZRP5n" # Roy測試用機器人 對 Roy專屬的token
+    CHANNEL_NOTIFY_TOKEN = "z0bvCgU80dpOwxCRt6EGlKVnuChzDEnE3rQeTLEcydZ"  # Roy_Notify_測試機器人 對 Roy專屬的token
+
+    OFFICIAL_PUSH_API = "https://api.line.me/v2/bot/message/push"
+    OFFICIAL_NOTIFY_API = "https://notify-api.line.me/api/notify"
+
+    user_id = dict(
+        roykuo="U9afa5683614c2f30296a92eb07984d57"
+    )
+    push_header = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': f'Bearer {CHANNEL_ACCESS_TOKEN}'}
+
+    notify_header = {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Authorization': f'Bearer {CHANNEL_NOTIFY_TOKEN}'}
+    # line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+    # handler = WebhookHandler(CHANNEL_SECRET_TOKEN)
+
 
 class Constant:
     CODE_TYPE_ISSUE_STATUS = 0
--- app/main/controller/linebot_controller.py
+++ app/main/controller/linebot_controller.py
@@ -1,30 +1,80 @@
 from app.main.dto.thelinebot import LineBotDto
 from app.main.service import ret
+from app.main.constant import LineConstant
 from app.main.util.common import (aaa_verify, api_exception_handler,
                                   check_access_authority)
 from flask import request
+import requests as urllib_requests
 from flask_api import status
 from flask_restplus import Resource
-from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from werkzeug.exceptions import NotFound
+# from linebot import LineBotApi, WebhookHandler
+# from linebot.models import MessageEvent, TextMessage, TextSendMessage, messages
+# from werkzeug.exceptions import NotFound
 
 api = LineBotDto.api
 response_status = {status.HTTP_200_OK: ret.get_code_full(ret.RET_OK),
                    status.HTTP_401_UNAUTHORIZED: ret.get_code_full(ret.RET_NO_CUST_ID),
                    status.HTTP_404_NOT_FOUND: ret.get_code_full(ret.RET_NOT_FOUND)}
 
-CHANNEL_ACCESS_TOKEN = """
-qipp53w9dsKIjaDG3D5eYswChigJUmYdgD6ilha3BCHjF4rJmG8dVjj3kMqpBy4TvTnYODobZelFc5bsSz9ycEx09y
-/XU3aZO42Bp2o0+9f9TRJBFMeUih6Oi2YB77ET4+u5z/miOF5FRihh5ubRTgdB04t89/1O/w1cDnyilFU=
-"""
 
-CHANNEL_SECRET_TOKEN = """
-6fd6a21c86d311aaf115d9588cc5fc46
-"""
+@api.route("/notify")
+class LineNotify(Resource):
+    def post(self):
+        """ connecting line notify to send free messeage """
+        payload = request.json
+        print("------------")
+        print(payload)  # for debug
+        print("------------")
+        response = {"hint": "已接收請求，但內容為空"}
+        if payload.get("events"):
+            temp = payload.get("events")[0]
+            text = temp.get("text")
+            json_for_msg = dict(
+                message=text  # HINT 注意這裡跟line push API的messages不一樣
+            )
+            result = urllib_requests.post(
+                LineConstant.OFFICIAL_NOTIFY_API,
+                headers=LineConstant.notify_header,
+                params=json_for_msg)
+            if result.status_code == 200:
+                response = {"hint": "訊息發送成功"}
 
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(CHANNEL_SECRET_TOKEN)
+        # if payload.get("events"):
+        return ret.http_resp(ret.RET_OK, extra=response), status.HTTP_200_OK
+
+
+@api.route("/push")
+class Push(Resource):
+    # @api_exception_handler
+    def post(self):
+        """ connecting line bot API to push messeage """
+        payload = request.json
+
+        print("------------")
+        print(payload)  # for debug
+        print("------------")
+        response = {"hint": "已接收請求，但內容為空"}
+        if payload.get("events"):
+            temp = payload.get("events")[0]
+            nickname = temp.get("nickname")
+            text = temp.get("text")
+            json_for_msg = dict(
+                to=LineConstant.user_id.get(nickname),
+                messages=[{
+                    "type": "text",
+                    "text": text
+                }]
+            )
+
+            result = urllib_requests.post(
+                LineConstant.OFFICIAL_PUSH_API,
+                headers=LineConstant.push_header,
+                json=json_for_msg)
+            if result.status_code == 200:
+                response = {"hint": "訊息發送成功"}
+
+        # if payload.get("events"):
+        return ret.http_resp(ret.RET_OK, extra=response), status.HTTP_200_OK
 
 
 @api.route("/webhook")
