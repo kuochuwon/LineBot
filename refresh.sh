097bb1a0cea3d975a53f1be491e974b946fe5447
Roykuo <kuochuwon@gmail.com>
2021-04-22 00:42:23 +0800

    for handle idle, making web traffic

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,12 @@
-# prevent the dyno idling!
-#!/bin/bash
-#d=$(date +%Y-%m-%d)
-d=$(date)
-echo "current time: $d"
+#!/bin/sh
+# script to unidle heroku installation for the use with cronjob
+# usage in crontab:
+# */5 11-15 * * 1-5 /usr/local/bin/uptimer.sh http://www.example.com
+# The command /usr/local/bin/uptimer.sh http://www.example.com will execute every 5th minute of 11am through 3pm Mondays through Fridays in every month.
+# resources: http://www.cronchecker.net
+echo url to unidle: $1
+echo [UPTIMER]: waking up at:
+date
+curl $1
+echo [UPTIMER]: awake at:
+date
