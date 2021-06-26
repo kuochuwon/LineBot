1477d3bd2eaff40c9766e47762362a946b4bec60
Roykuo <kuochuwon@gmail.com>
2021-06-26 18:37:58 +0800

    update unittest and move function position

--- app/main/service/line_flex_message.py
+++ app/main/service/line_flex_message.py
@@ -12,17 +12,8 @@ def get_general_carousel(filename) -> list:
     return expect
 
 
-# TODO 產生輪播內容
-def sending_church_carousel_by_reply(replytoken):
-    try:
-        flex_message = get_general_carousel("church_homepage.json")
-        general_reply(replytoken, [flex_message])
-    except Exception as e:
-        logger.exception(f"sending carousel failed {e}")
-        raise
-
-
-def sending_tutorial(replytoken):
+# HINT 此段是為了將原函式和lineAPI分離，以便進行單元測試
+def get_tutorial() -> dict:
     pic_1 = "https://i.imgur.com/nYq2Aud.png"
     pic_2 = "https://i.imgur.com/SutrRum.png"
     tutorial_1 = {
@@ -35,6 +26,33 @@ def sending_tutorial(replytoken):
         "originalContentUrl": pic_2,
         "previewImageUrl": pic_2
     }
+    return tutorial_1, tutorial_2
+
+
+# TODO 產生輪播內容
+def sending_church_carousel_by_reply(replytoken):
+    try:
+        flex_message = get_general_carousel("church_homepage.json")
+        general_reply(replytoken, [flex_message])
+    except Exception as e:
+        logger.exception(f"sending carousel failed {e}")
+        raise
+
+
+def sending_tutorial(replytoken):
+    # pic_1 = "https://i.imgur.com/nYq2Aud.png"
+    # pic_2 = "https://i.imgur.com/SutrRum.png"
+    # tutorial_1 = {
+    #     "type": "image",
+    #     "originalContentUrl": pic_1,
+    #     "previewImageUrl": pic_1
+    # }
+    # tutorial_2 = {
+    #     "type": "image",
+    #     "originalContentUrl": pic_2,
+    #     "previewImageUrl": pic_2
+    # }
+    tutorial_1, tutorial_2 = get_tutorial()
     general_reply(replytoken, [tutorial_1, tutorial_2])
     logger.debug("觸發教學流程")
 
--- test/test_mainfunction.py
+++ test/test_mainfunction.py
@@ -1,12 +1,28 @@
 from test.base import BaseTestCase2
-from app.main.service.line_flex_message import get_general_carousel
+from app.main.service.line_flex_message import get_general_carousel, get_tutorial
+
+first_layer_keyword = {"type", "hero", "body", "size", "footer"}  # set
 
 
 class TestMainFunction(BaseTestCase2):
     def test_church_carousel(self):
-        keyword_set = {"type", "hero", "body", "size", "footer"}  # set
+        # keyword_set = {"type", "hero", "body", "size", "footer"}  # set
         for_checking = get_general_carousel("church_homepage.json").get("contents")
         carousel_objs = for_checking.get("contents")
         for each in carousel_objs:
-            boo_res = set(each.keys()).issubset(keyword_set)  # HINT check each key is valid or not
+            boo_res = set(each.keys()).issubset(first_layer_keyword)  # HINT check each key is valid or not
             self.assertTrue(boo_res)
+        # self.check_first_layer(carousel_objs)
+
+    def test_bible_sentence(self):
+        for_checking = get_general_carousel("daily_bibile.json").get("contents")
+        boo_res = set(for_checking.keys()).issubset(first_layer_keyword)
+        self.assertTrue(boo_res)
+
+    def test_tutorial_page(self):
+        image_set = ["type", "originalContentUrl", "previewImageUrl"]
+        for_checking1, for_checking2 = get_tutorial()
+        boo_res1 = set(for_checking1.keys()).issubset(image_set)
+        boo_res2 = set(for_checking2.keys()).issubset(image_set)
+        self.assertTrue(boo_res1)
+        self.assertTrue(boo_res2)
